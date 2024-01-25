35ff9bef8796961cab4ca0ec57c54990881a44d5
wtckk <nikita.shutnikov89@gmail.com>
2024-01-25 10:17:52 +0500

    init

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as process from "process";
+
+async function start() {
+  const PORT = process.env.PORT || 5000
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+}
+
+start();
